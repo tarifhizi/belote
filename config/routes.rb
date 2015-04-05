fb48b0d1f36571a2ec272e5b4273bd4eb267c4c1
tarif hizi <tarifhizi@gmail.com>
2015-04-05 20:20:36 +0000

    added pages and devise for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/belote'
+
   resources :players
+  
+  root to: 'pages#home' 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
